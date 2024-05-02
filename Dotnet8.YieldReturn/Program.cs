365976e622d822b7ea61fa508dd1fe3ef2f17bbc
mgchit <chitlunh@gmail.com>
2024-05-02 12:49:19 +0630

    Add project files.

---
+++ Dotnet8.YieldReturn/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+await foreach (int n in GenerateNumbersAsync(5))
+{
+    Console.Write(n);
+    Console.Write(" ");
+}
+// Output: 0 2 4 6 8
+
+async IAsyncEnumerable<int> GenerateNumbersAsync(int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        yield return await ProduceNumberAsync(i);
+    }
+}
+
+async Task<int> ProduceNumberAsync(int seed)
+{
+    await Task.Delay(1000);
+    return 2 * seed;
+}
